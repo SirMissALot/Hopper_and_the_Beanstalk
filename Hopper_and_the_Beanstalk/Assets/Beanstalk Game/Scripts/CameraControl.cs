ab57219bd20cab7a41ba53f3dbd4e4ad210159c0
Giane Carlo Go <gcarlo_1990@yahoo.com>
2018-11-15 06:55:09 -0500

    Update Controller

---
+++ Hopper_and_the_Beanstalk/Assets/Beanstalk Game/Scripts/CameraControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform target;
+	public float lookSmooth = 0.09f;
+	public Vector3 offset = new Vector3(0,6,-8);
+	public float xTilt = 10f;
+	Vector3 destination = Vector3.zero;
+	Controller m_controller;
+	float rotateVel = 0;
+
+	// Use this for initialization
+	void Start () {
+		SetCameraTarget(target);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetCameraTarget(Transform t){
+		target = t;
+
+		m_controller = target.GetComponent<Controller>();
+	}
+
+	void LateUpdate(){
+
+		MoveToTarget();
+		LookAtTarget();
+	}
+
+	void MoveToTarget(){
+		destination = m_controller.TargetRotation * offset;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget(){
+		float eulerYAngle = Mathf.SmoothDamp(transform.eulerAngles.y,target.eulerAngles.y,ref rotateVel, lookSmooth);
+		transform.rotation = Quaternion.Euler(transform.eulerAngles.x,eulerYAngle,0);
+	}
+
+	 
+}
---
+++ Hopper_and_the_Beanstalk/Assets/Beanstalk Game/Scripts/Controller.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public float inputDelay = 0.1f;
+	public float fowardVel = 12;
+	public float rotateVel = 100;
+	public float jumpForce = 25;
+	public float distanceToGrounded = 0.1f;
+	public LayerMask ground;
+	public float gravity = 0.75f;
+	public VirtualJoystick joystick;
+	Animator m_anim;
+	Quaternion targetRotation;
+	Rigidbody rBody;
+	Vector3 velocity = Vector3.zero;
+	float forwardInput, turnInput, jumpInput;
+	public Quaternion TargetRotation{ get{ return targetRotation; }}
+	bool Grounded(){
+		return Physics.Raycast(transform.position,Vector3.down,distanceToGrounded,ground);
+	}
+	// Use this for initialization
+	void Start () {
+		targetRotation = transform.rotation;
+		rBody = GetComponent<Rigidbody>();
+		m_anim = GetComponent<Animator>();
+
+		forwardInput = turnInput = jumpInput = 0;
+	}
+
+	void GetInput(){
+		forwardInput = joystick.Vertical();
+		turnInput = joystick.Horizontal();
+		jumpInput = Input.GetAxisRaw("Jump");
+
+		m_anim.SetFloat("VelX", forwardInput);
+		m_anim.SetFloat("VelY", turnInput);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput();
+		Turn();
+	}
+
+	void FixedUpdate (){
+		Move();
+		Jump();
+
+		rBody.velocity = transform.TransformDirection(velocity);
+	}
+
+	void Move(){
+		if(Mathf.Abs(forwardInput) > inputDelay){
+			// rBody.velocity = transform.forward * forwardInput * fowardVel;
+			velocity.z = fowardVel * forwardInput;
+		} else {
+			// rBody.velocity = Vector3.zero;
+			velocity.z = 0;
+		}
+	}
+
+	void Turn(){
+
+		if(Mathf.Abs(turnInput) > inputDelay){
+			targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		}
+		transform.rotation = targetRotation;
+	}
+
+	void Jump(){
+		if(jumpInput> 0 && Grounded()){
+			velocity.y = jumpForce;
+			m_anim.SetBool("Jump", true);
+		}
+		else if(jumpInput == 0 && Grounded()){
+			velocity.y = 0;
+		}
+		else{
+			velocity.y -= gravity;
+		}
+	}
+
+		
+}
