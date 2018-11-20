f77bcaf6249dd71fa978dbd5eaea62c037025d5b
Giane Carlo Go <gcarlo_1990@yahoo.com>
2018-11-19 19:07:31 -0500

    Camera Collision

--- Hopper_and_the_Beanstalk/Assets/Beanstalk Game/Scripts/Controller.cs
+++ Hopper_and_the_Beanstalk/Assets/Beanstalk Game/Scripts/Controller.cs
@@ -8,6 +8,7 @@ public class Controller : MonoBehaviour {
 	public float fowardVel = 12;
 	public float rotateVel = 100;
 	public float jumpForce = 25;
+	public bool isGrounded;
 	public float distanceToGrounded = 0.1f;
 	public LayerMask ground;
 	public float gravity = 0.75f;
@@ -16,21 +17,44 @@ public class Controller : MonoBehaviour {
 	Quaternion targetRotation;
 	Rigidbody rBody;
 	Vector3 velocity = Vector3.zero;
+
 	float forwardInput, turnInput, jumpInput;
 	public Quaternion TargetRotation{ get{ return targetRotation; }}
+
 	bool Grounded(){
 		return Physics.Raycast(transform.position,Vector3.down,distanceToGrounded,ground);
 	}
+	
 	// Use this for initialization
 	void Start () {
 		targetRotation = transform.rotation;
 		rBody = GetComponent<Rigidbody>();
 		m_anim = GetComponent<Animator>();
-
 		forwardInput = turnInput = jumpInput = 0;
+		isGrounded = true;
+
+
 	}
 
+	// Update is called once per frame
+	void Update () {
+
+		GetInput();
+		Turn();
+
+	}
+
+	void FixedUpdate (){
+
+		Move();
+		Jump();
+
+		rBody.velocity = transform.TransformDirection(velocity);
+	}
+
+	// Input Check
 	void GetInput(){
+		
 		forwardInput = joystick.Vertical();
 		turnInput = joystick.Horizontal();
 		jumpInput = Input.GetAxisRaw("Jump");
@@ -38,20 +62,8 @@ public class Controller : MonoBehaviour {
 		m_anim.SetFloat("VelX", forwardInput);
 		m_anim.SetFloat("VelY", turnInput);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		GetInput();
-		Turn();
-	}
 
-	void FixedUpdate (){
-		Move();
-		Jump();
-
-		rBody.velocity = transform.TransformDirection(velocity);
-	}
-
+	// Movement + Turn Input
 	void Move(){
 		if(Mathf.Abs(forwardInput) > inputDelay){
 			// rBody.velocity = transform.forward * forwardInput * fowardVel;
@@ -70,12 +82,13 @@ public class Controller : MonoBehaviour {
 		transform.rotation = targetRotation;
 	}
 
+	// Jump Input
 	void Jump(){
-		if(jumpInput> 0 && Grounded()){
+		if(jumpInput> 0 && isGrounded && !m_anim.GetCurrentAnimatorStateInfo(0).IsName("Jump")){
 			velocity.y = jumpForce;
 			m_anim.SetBool("Jump", true);
 		}
-		else if(jumpInput == 0 && Grounded()){
+		else if(jumpInput == 0 && isGrounded){
 			velocity.y = 0;
 			m_anim.SetBool("Jump", false);
 		}
@@ -83,6 +96,21 @@ public class Controller : MonoBehaviour {
 			velocity.y -= gravity;
 		}
 	}
+
+	// Ground check
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Ground"){
+			isGrounded = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "Ground"){
+			isGrounded = false;
+		}
+	}
+
+	
 
 		
 }
---
+++ Hopper_and_the_Beanstalk/Assets/Beanstalk Game/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	[SerializeField]Vector3 offset;
+	[SerializeField]float damping;
+	Transform cameraLookTarget;
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		cameraLookTarget = Player.transform.Find("default");
+		if(cameraLookTarget == null){
+			cameraLookTarget = Player.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 targetPosition = cameraLookTarget.position + Player.transform.forward * offset.z + 
+												   Player.transform.up * offset.y + 
+												   Player.transform.right * offset.x;
+
+		Quaternion targetRotation = Quaternion.LookRotation(cameraLookTarget.position - targetPosition,Vector3.up);	
+
+        Vector3 collisionCheckEnd = cameraLookTarget.position;
+
+        Debug.DrawLine(targetPosition,collisionCheckEnd,Color.red);
+
+        RaycastHit hit;
+        if(Physics.Linecast(collisionCheckEnd,targetPosition,out hit)){
+
+			Vector3 hitpoints = new Vector3(hit.point.x + hit.normal.x *.2f ,hit.point.y,hit.point.z + hit.normal.z * .2f);
+            targetPosition = new Vector3(hitpoints.x,targetPosition.y,hitpoints.z);
+        }									   
+
+		transform.position = Vector3.Lerp(transform.position, targetPosition, damping * Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, damping);										   
+	}
+
+}
+
